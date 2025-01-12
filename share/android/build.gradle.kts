c53a18676bd7c7c6c80fde9b0d3cae7cbeb735cf
liufeias <675952640@qq.com>
2025-01-12 22:05:31 +0800

    Update build.gradle.kts

--- share/android/build.gradle.kts
+++ share/android/build.gradle.kts
@@ -1,7 +1,17 @@
 plugins {
     alias(libs.plugins.agp.lib)
 }
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // 目标 JVM 版本
+    }
+}
 
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17" // 与 Java 保持一致
+    }
+}
 android {
     namespace = "org.lsposed.lspatch.share"
 
